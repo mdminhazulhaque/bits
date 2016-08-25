61a3838a3bc6970e3c8b444851b7fa8262258b43
Md Minhazul Haque <mdminhazulhaque@gmail.com>
2016-08-25 23:48:30 +0600

    Changed font, added JSON links, updated Meta

--- etc/favicon.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-inkscape favicon.svg -w 144 -h 144 -e apple-touch-icon-144-precomposed.png 
-inkscape favicon.svg -w 16 -h 16 -e favicon.png
-convert favicon.png favicon.ico
-rm favicon.png 
