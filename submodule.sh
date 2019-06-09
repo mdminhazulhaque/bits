95bbdaf1f36a0b0eac4aba397c1a57ca83693dd1
Minhaz <mdminhazulhaque@gmail.com>
2019-06-09 17:46:30 +0800

    Do customization over submodules

---
+++ submodule.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# get poole sass files
+cp -r poole/_sass .
+cp -r poole/styles.scss public/css/poole.scss
+
+# get hyde theme
+head -4 public/css/poole.scss > public/css/hyde.scss
+cat hyde/public/css/hyde.css >> public/css/hyde.scss
+
+# get pygment theme
+head -4 public/css/poole.scss > public/css/syntax.scss
+cat pygments-css/default.css >> public/css/syntax.scss
