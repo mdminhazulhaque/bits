60aecc09f11137a38cd46611edb6798741107bed
Md Minhazul Haque <mdminhazulhaque@gmail.com>
2020-04-16 15:51:17 +0800

    MCO

--- build.sh
+++ build.sh
@@ -8,6 +8,7 @@ rm _site/README
 
 cp -r _site/* $GH_PAGES/
 cd $GH_PAGES
+sed -i "s/http:\/\/localhost:4000/https:\/\/bits.mdminhazulhaque.io/g" docs/sitemap.xml
 git add .
 git commit -m "New files"
 git push -u origin gh-pages
